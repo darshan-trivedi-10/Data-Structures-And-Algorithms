cc5b1b722af6bda5a2296811da4a95180bbcae25
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-08-27 18:13:30 +0530

    Update 6_C_Vacation.cpp

--- Dp/6_C_Vacation.cpp
+++ Dp/6_C_Vacation.cpp
@@ -1,58 +1,86 @@
-// https://atcoder.jp/contests/dp/tasks/dp_c
 #include <bits/stdc++.h>
 using namespace std;
 using lli = long long int;
 using ll = long long;
 #define endl '\n'
+
 struct triplet
 {
     int a, b, c;
 };
 
-// Time Complexity : O(n x 4 x 2).
-int solve(vector<vector<int>> &points, vector<vector<int>> &dp, int day, int flag)
+int getHappiness(triplet happiness, int i)
 {
-    if (dp[day][flag] != -1)
+    return (i == 0 ? happiness.a : (i == 1 ? happiness.b : happiness.c));
+}
+
+// Recursion + Memorizastion
+int findMaxHappiness(vector<triplet> &arr, vector<vector<int>> &dp, int idx, int prevActivity, int n)
+{
+    if (idx == n)
     {
-        return dp[day][flag];
+        return 0;
     }
 
-    if (day == 0)
+    if (dp[idx][prevActivity] != -1)
     {
-        int maxi = 0;
-        for (int i = 0; i <= 2; i++)
+        return dp[idx][prevActivity];
+    }
+
+    int maxHappines = INT_MIN;
+    for (int i = 0; i < 3; i++)
+    {
+        if (prevActivity != i)
         {
-            if (i != flag)
+            int currHappiness = findMaxHappiness(arr, dp, idx + 1, i, n) + getHappiness(arr[idx], i);
+            maxHappines = max(maxHappines, currHappiness);
+        }
+    }
+
+    return dp[idx][prevActivity] = maxHappines;
+}
+
+
+//  Using Tabulastion
+int findMaxHappiness(vector<triplet> &arr, vector<vector<int>> &dp, int n){
+    
+    vector<vector<int>> dp(n, vector<int>(4, -1));
+    dp[0][0] = max(arr[0][1], arr[0][2]);
+    dp[0][1] = max(arr[0][0], arr[0][2]);
+    dp[0][2] = max(arr[0][0], arr[0][1]);
+    dp[0][3] = max({arr[0][0], arr[0][1], arr[0][2]});
+
+    for (int day = 1; day < n; day++)
+    {
+        for (int last = 0; last < 4; last++)
+        {
+            dp[day][last] = 0;
+            for (int task = 0; task < 3; task++)
             {
-                maxi = max(maxi, points[day][i]);
+                if (task != last)
+                {
+                    int point = arr[day][task] + dp[day - 1][task];
+                    dp[day][last] = max(dp[day][last], point);
+                }
             }
         }
-        return dp[day][flag] = maxi;
     }
 
-    int maxi = 0;
-    for (int i = 0; i <= 2; i++)
-    {
-        if (i != flag)
-        {
-            maxi = max(maxi, points[day][i] + solve(points, dp, day - 1, i));
-        }
-    }
-    return dp[day][flag] = maxi;
+    return dp[n - 1][3];
 }
 
 int main()
 {
     int n;
     cin >> n;
-    vector<vector<int>> points(n, vector<int>(3));
-    vector<vector<int>> dp(n, vector<int>(4, -1));
+    vector<triplet> arr(n);
     for (int i = 0; i < n; i++)
     {
-        cin >> points[i][0] >> points[i][1] >> points[i][2];
+        cin >> arr[i].a >> arr[i].b >> arr[i].c;
     }
 
-    cout << solve(points, dp, n - 1, 3) << endl;
+    vector<vector<int>> dp(n, vector<int>(4, -1));
+    cout << findMaxHappiness(arr, dp, 0, 3, n);
 
     return 0;
 }
